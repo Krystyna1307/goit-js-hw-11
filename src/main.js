25bf4fb167146e30ad8e7ece8e16589de2d7594a
Krystyna1307 <kristina.dudnik8@gmail.com>
2024-09-23 12:46:04 +0200

    render f

--- src/main.js
+++ src/main.js
@@ -6,6 +6,9 @@ import "simplelightbox/dist/simple-lightbox.min.css";
 
 import { BASE_URL, API_KEY } from "./js/pixabay-api";
 
+import createGalleryMarkup from './js/render-functions.js';
+
+
 const form = document.querySelector('.js-search-form');
 const gallery = document.querySelector('.gallery');
 
@@ -70,31 +73,4 @@ function onSubmit(event) {
         .catch(error => {
             console.log(error);
         });
-}
-
-function createGalleryMarkup(gallery) {
-    return gallery
-        .map(photo => {
-            return `<li class="li-gallery">
-            <div class="img-box">
-                <a class="gallery-link" href="${photo.largeImageURL}">
-            <img
-            class="gallery-image"
-            src="${photo.webformatURL}"
-            alt="${photo.tags}"
-            title="${photo.tags}"
-            /></a></div>
-        <div class="description-box">
-            <div class="param-boxes"><p class="parameters">Likes</p>
-            <p class="values">${photo.likes}</p></div>
-            <div class="param-boxes"><p class="parameters">Views</p>
-            <p class="values">${photo.views}</p></div>
-            <div class="param-boxes"><p class="parameters">Comments</p>
-            <p class="values">${photo.comments}</p></div>
-            <div class="param-boxes"><p class="parameters">Downloads</p>
-            <p class="values">${photo.downloads}</p></div>
-        </div>
-        </li>`;
-    })
-    .join('');
 }
